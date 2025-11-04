be07410612f28c1ff16d6fdca2f2920cb9843044
fredrawi <fredrikandreas03@gmail.com>
2025-11-04 11:32:39 +0100

    Updated CMakeLists and now everything compiles and works

---
+++ labs/lab2/main.cpp
@@ -0,0 +1,24 @@
+// #include area (SECTION 1)
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    // GLFW initialization code (SECTION 2)
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    // OpenGL initialization code (SECTION 3)
+
+    // OpenGL data transfer code (SECTION 4)
+
+    // Application loop code (SECTION 5)
+
+    // Termination code (SECTION 6)
+    return 0;
+}
