b1f982ef6d9772ff4495efa8f208a3ff8ecb747d
fredrawi <fredrikandreas03@gmail.com>
2025-11-04 15:55:54 +0100

    Completed issue #5

--- framework/GeometricTools/GeometricTools.h
+++ framework/GeometricTools/GeometricTools.h
@@ -50,6 +50,7 @@ namespace GeometricTools
                 vertices[index++] = y;
             }
         }
+
     public:
         static constexpr int TotalFloats = 4 * (DivX + DivY + 2);
         static constexpr int TotalVertices = TotalFloats / 2;
@@ -66,4 +67,52 @@ namespace GeometricTools
             return vertices;
         }
     }
+
+    template <int DivX, int DivY>
+    class UnitGridTopologyTriangles
+    {
+    private:
+        void GenerateIndices()
+        {
+            int index = 0;
+
+            for (int y = 0; y < DivY; ++y)
+            {
+                for (int x = 0; x < DivX; ++x)
+                {
+                    int tL = y * VerticesPerRow + x;
+                    int tR = tL + 1;
+                    int bL = tL + VerticesPerRow;
+                    int bR = bL + 1;
+
+                    // First triangle
+                    indices[index++] = tL;
+                    indices[index++] = bL;
+                    indices[index++] = bR;
+
+                    // Second triangle
+                    indices[index++] = tL;
+                    indices[index++] = bR;
+                    indices[index++] = tR;
+                }
+            }
+        }
+
+    public:
+        static constexpr int VerticesPerRow = DivX + 1;
+        static constexpr int NumIndices = 6 * DivX * DivY;
+
+        std::array<GLuint, NumIndices> indices{};
+
+        UnitGridTopologyTriangles()
+        {
+            GenerateIndices();
+        }
+
+        const std::array<GLuint, NumIndices> &GetIndices() const
+        {
+            return indices;
+        }
+    };
+
 }
