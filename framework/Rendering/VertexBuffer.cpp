2e3c560175cc66ccb8558bf13024ba4172899ce2
fredrawi <fredrikandreas03@gmail.com>
2025-11-10 10:48:39 +0100

    Added VertexBuffer.cpp to implement VertexBuffer.h, issue #7

---
+++ framework/Rendering/VertexBuffer.cpp
@@ -0,0 +1,31 @@
+#include "VertexBuffer.h"
+#include <glad/glad.h>
+
+VertexBuffer::VertexBuffer(const void *vertices, GLsizei size)
+{
+    glGenBuffers(1, &VertexBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), vertices, GL_STATIC_DRAW);
+}
+
+VertexBuffer::~VertexBuffer()
+{
+    Unbind();
+    glDeleteBuffers(1, &VertexBufferID);
+}
+
+void VertexBuffer::Bind() const
+{
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
+}
+
+void VertexBuffer::Unbind() const
+{
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void VertexBuffer::BufferSubData(GLintptr offset, GLsizeiptr size, const void *data) const
+{
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
+    glBufferSubData(GL_ARRAY_BUFFER, offset, size, data);
+}
