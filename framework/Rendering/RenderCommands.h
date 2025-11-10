c714a8be45af9f539bbf3ed4e8550029d18b72cd
fredrawi <fredrikandreas03@gmail.com>
2025-11-10 15:22:59 +0100

    Completed task 2 for issue #13

--- framework/Rendering/RenderCommands.h
+++ framework/Rendering/RenderCommands.h
@@ -1,4 +1,5 @@
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 #include <memory>
 #include "VertexArray.h"
@@ -18,5 +19,20 @@ namespace RenderCommands
     inline void DrawIndex(const std::shared_ptr<VertexArray> &vao, GLenum primitive)
     {
         glDrawElements(primitive, vao->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+    }
+
+    inline void SetClearColor(glm::vec4 color)
+    {
+        glClearColor(color.r, color.g, color.b, color.a);
+    }
+
+    inline void SetWireframeMode()
+    {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+
+    inline void SetSolidMode()
+    {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
 }
